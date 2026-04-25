e4ce7c09aac9eca155b12ec2c0f121e0c22378ad
Santiago Borja <45884381+sborjar@users.noreply.github.com>
2026-04-25 01:32:44 -0500

    Update extract.py

--- src/extract.py
+++ src/extract.py
@@ -37,31 +37,34 @@ def loaddata(date1):
     cursor.execute(query, param)
     rows = cursor.fetchall()
 
-    end_time = time.perf_counter()
-
-    elapsed_time1 = end_time - start_time
-
-    log(f'Elapsed query = {elapsed_time1} seconds')
-
-    start_time = time.perf_counter()
-
-    file = f"data/calls_{date1}.csv"
-    with open(file, 'w', newline='', encoding='utf-8') as f:
-       writer = csv.writer(f)
-                      
-       # Write header (column names)
-       column_headers = [i[0] for i in cursor.description]
-       writer.writerow(column_headers)
-                                                                               
-       # Write data rows
-       writer.writerows(rows)
-
-    end_time = time.perf_counter()
-    elapsed_time2 = end_time - start_time
-
-    log(f"Row exported = {len(rows)} rows")
-    log(f"Elapsed exported = {elapsed_time2} seconds")
+    if len(rows)>0:
+        end_time = time.perf_counter()
     
+        elapsed_time1 = end_time - start_time
+    
+        log(f'Elapsed query = {elapsed_time1} seconds')
+    
+        start_time = time.perf_counter()
+    
+        file = f"data/calls_{date1}.csv"
+        with open(file, 'w', newline='', encoding='utf-8') as f:
+           writer = csv.writer(f)
+                          
+           # Write header (column names)
+           column_headers = [i[0] for i in cursor.description]
+           writer.writerow(column_headers)
+                                                                                   
+           # Write data rows
+           writer.writerows(rows)
+    
+        end_time = time.perf_counter()
+        elapsed_time2 = end_time - start_time
+    
+        log(f"Row exported = {len(rows)} rows")
+        log(f"Elapsed exported = {elapsed_time2} seconds")
+    else:
+        log(f"Query result is empty")
+        
     cursor.close()
     db.close()
     
