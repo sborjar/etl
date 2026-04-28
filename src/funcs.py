de0462a9a6227f4be6062361bed2de8cbe5fb2bf
Santiago Borja Romero <santiago@callevo.com>
2026-04-28 03:29:06 -0500

    FIX funcion log archivo log por fecha

--- src/funcs.py
+++ src/funcs.py
@@ -1,8 +1,18 @@
 
 import logging
+from datetime import datetime
+import os
+
+if not os.path.exists("logs"):
+    os.makedirs("logs")
 
 logger = logging.getLogger("callevo")
-logging.basicConfig(filename='logs/etl.log', level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+current_date = datetime.now().strftime("%Y-%m-%d")
+logging.basicConfig(
+    filename=f'logs/etl_{current_date}.log', 
+    level=logging.INFO, 
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
 
 def log(msg):
     print(msg)
